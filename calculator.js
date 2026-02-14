9ac060f644c1761f71a4d123c4ab1910bfea972d
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-14 17:17:49 -0300

    Biblioteca instalada e erro percebido

--- calculator.js
+++ calculator.js
@@ -1,4 +1,20 @@
-number1 = Number(prompt('Digite um número: '))
-number2 = Number(prompt('Digite outro número: '))
+const prompt = require('prompt-sync')();
 
-operator = prompt('Digite um operador: ')
+let number1 = Number(prompt('Digite um número: '));
+let number2 = Number(prompt('Digite outro número: '));
+
+let question = prompt('Você quer entrar ou sair? (S/N) ').toUpperCase();
+
+while (question === 'S') {
+    let operator = prompt('Digite um operador (+, -, *, /): ');
+
+    if (operator === '+') {
+        console.log(number1 + number2);
+    } else if (operator === '-') {
+        console.log(number1 - number2);
+    } else if (operator === '*') {
+        console.log(number1 * number2);
+    } else if (operator === '/') {
+        console.log(number1 / number2);
+    };
+}
